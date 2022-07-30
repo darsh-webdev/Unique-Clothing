5376c531d0f4e3a7dabed2bb324c81f27f7c7766
Darshan Rajput <darshanrajput@Darshans-MacBook-Pro.local>
2022-07-30 23:44:14 +0530

    Added Checkout page with checkout items along with checkout cart functionality

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "../button/button.component";
 
 import { CartContext } from "../../contexts/cart.context";
@@ -7,7 +8,13 @@ import CartItem from "../cart-item/cart-item.component";
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
-  const { cartItems } = useContext(CartContext);
+  const { cartItems, isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate("/checkout");
+    setIsCartOpen(!isCartOpen);
+  };
 
   return (
     <div className="cart-dropdown-container">
@@ -16,7 +23,9 @@ const CartDropdown = () => {
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button type="button" onClick={goToCheckoutHandler}>
+        GO TO CHECKOUT
+      </Button>
     </div>
   );
 };
